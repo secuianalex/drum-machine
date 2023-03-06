ea25997fbc3970e80bd76efe8f9b85b1733af605
secuianalex <secuianalex@gmail.com>
2023-03-06 13:20:18 +0000

    Git init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Mic from "./Mic";
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Mic garnitura="cartofi" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
